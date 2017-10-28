b0ea370553e8a0dfa79f632274e1461a83329273
Andrew Hausammann <ahausammann017@students.ccsnh.edu>
2017-10-28 00:11:25 -0400

    created program logic

--- python/rna-transcription/rna_transcription.py
+++ python/rna-transcription/rna_transcription.py
@@ -1,2 +1,17 @@
+from string import ascii_uppercase
+
+
 def to_rna(dna_strand):
     
+    dna_to_rna = {"A":"U","C":"G","G":"C","T":"A","U":"A"}
+    rna_strand = ""
+    
+    for letter in str(dna_strand):
+        print(letter)
+        if letter.upper() in ascii_uppercase:
+            rna_strand += dna_to_rna[letter]
+        else:
+            continue
+    print(rna_strand)
+    return rna_strand
+
