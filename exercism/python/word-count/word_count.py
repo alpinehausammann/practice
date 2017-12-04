dc76ecb3731896c8af08965455824f85a2d094db
alpinehausammann <alpinehausammann@users.noreply.github.com>
2017-12-04 18:07:07 -0500

    completed word_count.py

--- exercism/python/word-count/word_count.py
+++ exercism/python/word-count/word_count.py
@@ -1,13 +1,19 @@
 import re
 def word_count(phrase):
     word_dict = {}
-    
-    for word in re.findall(r'[a-zA-Z][\'a-zA-Z]?', phrase):
+
+    for word in re.findall(r'[0-9a-zA-Z]+[\']?[0-9a-zA-Z]+', phrase.lower()):
         if word not in word_dict.keys():
             word_dict[word] = 1
         else:
             word_dict[word] += 1
+    for word in re.findall(r'[0-9]+', phrase.lower()):
+        if word not in word_dict.keys():
+            word_dict[word] = 1
+        else:
+            continue
     return word_dict
+    print(word_dict.items())
 
 
 word_count("hi there jimmy there hi they")
