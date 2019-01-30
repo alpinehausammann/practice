d965df4c431bf9610d7b537b5d1e1bbee19086b9
Andrew Hausammann <alpinehausammann@gmail.com>
2019-01-30 10:15:59 -0500

    Began work on rational_numbers.py

--- exercism/python/rational-numbers/rational_numbers.py
+++ exercism/python/rational-numbers/rational_numbers.py
@@ -3,8 +3,8 @@ from __future__ import division
 
 class Rational(object):
     def __init__(self, numer, denom):
-        self.numer = None
-        self.denom = None
+        self.numer = numer
+        self.denom = denom
 
     def __eq__(self, other):
         return self.numer == other.numer and self.denom == other.denom
@@ -13,13 +13,22 @@ class Rational(object):
         return '{}/{}'.format(self.numer, self.denom)
 
     def __add__(self, other):
-        pass
+    	numer = (self.numer * other.denom) + (other.numer * self.denom)
+    	denom = self.denom * other.denom
+    	numer, denum = self.negative_check(numer, denum)
+    	return Rational(numer, denom)
 
     def __sub__(self, other):
-        pass
+    	numer = (self.numer * other.denom) - (other.numer * self.denom)
+    	denom = self.denom * other.denom
+    	numer, denum = self.negative_check(numer, denum)
+    	return Rational(numer, denom)
 
     def __mul__(self, other):
-        pass
+    	numer = self.numer * other.numer
+    	denum = self.denom * other.denom
+    	numer, denom = self.negative_check(numer, denum)
+    	return Rational(numer, denom)
 
     def __truediv__(self, other):
         pass
@@ -31,4 +40,16 @@ class Rational(object):
         pass
 
     def __rpow__(self, base):
+    	
         pass
+        
+    def negative_check(self, numer, denum):
+        if numer < 0 and denum < 0:
+        	numer = numer * -1
+        	denum = denum * -1
+        	
+        return numer, denum
+        	
+if __name__ == "__main__":
+	x = Rational(1,2) * Rational(2,3)
+	print(x)
