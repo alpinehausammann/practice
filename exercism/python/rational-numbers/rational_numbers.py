80839c45e5b7d55b838ec4764481f70e884eda15
Andrew Hausammann <alpinehausammann@gmail.com>
2019-01-30 12:49:43 -0500

    added helper functions, edited whitespace

--- exercism/python/rational-numbers/rational_numbers.py
+++ exercism/python/rational-numbers/rational_numbers.py
@@ -2,54 +2,83 @@ from __future__ import division
 
 
 class Rational(object):
-    def __init__(self, numer, denom):
-        self.numer = numer
-        self.denom = denom
+  def __init__(self, numer, denom):
+    self.numer = numer
+    self.denom = denom
 
-    def __eq__(self, other):
-        return self.numer == other.numer and self.denom == other.denom
+  def __eq__(self, other):
+    return self.numer == other.numer and self.denom == other.denom
 
-    def __repr__(self):
-        return '{}/{}'.format(self.numer, self.denom)
+  def __repr__(self):
+    return '{}/{}'.format(self.numer, self.denom)
 
-    def __add__(self, other):
-    	numer = (self.numer * other.denom) + (other.numer * self.denom)
-    	denom = self.denom * other.denom
-    	numer, denum = self.negative_check(numer, denum)
-    	return Rational(numer, denom)
+  def __add__(self, other):
+    numer = (self.numer * other.denom) + (other.numer * self.denom)
+    denom = self.denom * other.denom
+    numer, denom = self.negative_check(numer, denom)
+    return self.gcd(numer, denom)
 
-    def __sub__(self, other):
-    	numer = (self.numer * other.denom) - (other.numer * self.denom)
-    	denom = self.denom * other.denom
-    	numer, denum = self.negative_check(numer, denum)
-    	return Rational(numer, denom)
+  def __sub__(self, other):
+    numer = (self.numer * other.denom) - (other.numer * self.denom)
+    denom = self.denom * other.denom
+    numer, denom = self.negative_check(numer, denom)
+    return self.gcd(numer, denom)
 
-    def __mul__(self, other):
-    	numer = self.numer * other.numer
-    	denum = self.denom * other.denom
-    	numer, denom = self.negative_check(numer, denum)
-    	return Rational(numer, denom)
+  def __mul__(self, other):
+    numer = self.numer * other.numer
+    denom = self.denom * other.denom
+    numer, denom = self.negative_check(numer, denom)
+    return self.gcd(numer, denom)
 
-    def __truediv__(self, other):
-        pass
+  def __truediv__(self, other):
+    numer = self.numer * other.denom
+    denom = other.numer * self.denom
+    numer, denom = self.negative_check(numer, denom)
+    return self.gcd(numer, denom)
 
-    def __abs__(self):
-        pass
+  def __abs__(self):
+    return self.gcd(self.numer * self.is_negative(self.numer),self.denom * self.is_negative(self.denom))
+    pass
 
-    def __pow__(self, power):
-        pass
+  def __pow__(self, power):
+    pass
 
-    def __rpow__(self, base):
+  def __rpow__(self, base):
     	
-        pass
-        
-    def negative_check(self, numer, denum):
-        if numer < 0 and denum < 0:
-        	numer = numer * -1
-        	denum = denum * -1
-        	
-        return numer, denum
-        	
+    pass
+
+  def negative_check(self, numer, denom):
+    if numer < 0 and denom < 0:
+      numer = numer * -1
+      denom = denom * -1
+      return numer, denom
+
+  def is_negative(self, num):
+    if num < 0:
+      return -1
+    return 1
+
+  def gcd(self, numer, denom):
+    negative = (self.is_negative(numer),self.is_negative(denom))
+    numer = numer * negative[0]
+    denom = denom * negative[1]
+
+    if numer < denom:
+      for i in range(numer, denom + 1):
+        if numer % i == 0 and denom % i == 0:
+          numer = (numer * negative[0]) / i
+          denom = (denom * negative[1]) / i
+    elif numer > denom:
+      for i in range(denom, numer + 1):
+        if numer % i == 0 and denom % i == 0:
+          numer = (numer * negative[0]) / i
+          denom = (denom * negative[1]) / i
+    else:
+      numer = 1
+      denom = 1
+		
+    return Rational(int(numer), int(denom))
+
 if __name__ == "__main__":
-	x = Rational(1,2) * Rational(2,3)
-	print(x)
+  x = abs(Rational(-1,2))
+  print(x)
